f9991a011ac5dfa3a024c55617f5e7f354c7c9cf
Ellie Park <elliepark1012@gmail.com>
2022-10-03 18:56:33 -0400

    final

--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,17 @@
-import React from "react";
-// import ListingCard from "./ListingCard";
 
-function ListingsContainer() {
+import React from "react";
+import ListingCard from "./ListingCard";
+
+function ListingsContainer({listings, removeListing}) {
+
+  const listingsComponents = listings.map(listing => {
+    return <ListingCard key={listing.id} listing={listing} removeListing={removeListing}/>
+  })
+
   return (
     <main>
       <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+        {listingsComponents}
       </ul>
     </main>
   );
